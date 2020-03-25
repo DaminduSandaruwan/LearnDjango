1f5c6af00930a01d6ae13df9849cf046672308b1
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-25 14:19:25 +0530

    Make Static files and create web Template

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include('calc.urls')),
+    path('', include('travello.urls')),
     path('admin/', admin.site.urls),
 ]
