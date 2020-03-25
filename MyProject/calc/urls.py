4d785216d20edb73c4cead19f461835861facc71
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-25 11:13:26 +0530

    Adding two numbers using GET

--- MyProject/calc/urls.py
+++ MyProject/calc/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('add',views.add,name='add')
+    
 ]
--- MyProject/calc/views.py
+++ MyProject/calc/views.py
@@ -9,5 +9,11 @@ def home(request):
 
     #return HttpResponse("<h1>Hello World</h1>")
    
+def add(request):
+    val1= request.GET['num1']
+    val2= request.GET['num2']
 
+    res = int(val1)+int(val2)
+
+    return render(request,"result.html" ,{'result' : res} )
 
