1a3f17916b6bbbe38d6619dc8dfadc21780559fa
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-27 13:24:27 +0530

    Logout

--- MyProject/acounts/urls.py
+++ MyProject/acounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register',views.register,name='register'),
-    path('login',views.login,name='login')
+    path('login',views.login,name='login'),
+    path("logout",views.logout,name="logout")
     
 ]
--- MyProject/acounts/views.py
+++ MyProject/acounts/views.py
@@ -53,3 +53,9 @@ def register(request):
     else:
         
         return render(request,'register.html')
+
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
